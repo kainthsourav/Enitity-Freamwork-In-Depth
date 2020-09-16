223fddf7065cde7ebd6c29006b54d27930254ad1
kainthsourav <souravkainth@outlook.com>
2020-09-16 16:49:54 +0530

    CodeFirst--demo

---
+++ CodeFirst/CodeFirst/Migrations/202009161029258_createPost.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class createPost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        DatePublished = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Body = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
