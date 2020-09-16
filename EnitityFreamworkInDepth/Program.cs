b7b4987e1b4ebc7cc523a51019f7286d748c2769
kainthsourav <souravkainth@outlook.com>
2020-09-16 12:00:07 +0530

    Add project files.

---
+++ EnitityFreamworkInDepth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFreamworkInDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new dbEFEntities();
+            var post = new dbo_Posts()
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                Postid = 1
+            };
+            context.dbo_Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
