5a7e85d0e4b2c2d3faaafae044b8c17474a34255
kainthsourav <souravkainth@outlook.com>
2020-09-18 11:03:18 +0530

    Linq queriers

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -27,6 +27,64 @@ namespace Queries
                 System.Console.WriteLine(item.Name);
             }
             System.Console.ReadLine();
+
+            //Restrication
+            var queryRes = from c in context.Courses
+                           where (c.Level == 1 && c.AuthorId==1)
+                           select (c);
+            //Ordering
+            var queryOrder = from c in context.Courses
+                             where c.Level == 1
+                             orderby c.Level, c.Name descending
+                             select c;
+
+            //groupby
+            var queryGroup = from c in context.Courses
+                             group c by c.Level
+                           into g
+                             select g;
+            foreach (var item in queryGroup)
+            {
+                System.Console.WriteLine(item.Key);
+                foreach (var i in item)
+                {
+                    System.Console.WriteLine("\t{0}", i.Name);
+                }
+            }
+
+            //joins
+            var queryJoin = from c in context.Courses
+                            join a in context.Authors on c.AuthorId equals a.Id
+                            select new { CourseName = c.Name, AuthorName = a.Name };
+
+            foreach (var item in queryJoin)
+            {
+                System.Console.WriteLine(item.AuthorName);
+                System.Console.WriteLine(item.CourseName);
+            }
+
+            //Group Join -left join
+            var queryGroupJoin=from a in context.Authors
+                               join c in context.Courses on a.Id equals c.AuthorId into g
+                               select new {AuhtorName=a.Name,CourseCout=g.Count()};
+                                
+                        foreach (var item in queryGroupJoin)
+            {
+                System.Console.WriteLine("{0} ({1})", item.AuhtorName, item.CourseCout);
+            }
+
+            //Cross Join
+            var queryCrossJoin = from a in context.Authors
+                                 from c in context.Courses
+                                 select new { AuthorName = a.Name, CourseName = c.Name };
+            foreach (var item in queryCrossJoin)
+            {
+                System.Console.WriteLine("{0} - {1}", item.AuthorName, item.CourseName); 
+            }
+
+
+
+            System.Console.ReadLine();
         }
     }
 }
