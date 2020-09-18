fd1d026f056f90828d5fcb15a4a649970863ff63
kainthsourav <souravkainth@outlook.com>
2020-09-18 09:07:41 +0530

    Fluent API

--- CodeFirstFromExistingDatabase/CodeFirstFromExistingDatabase/Migrations/202009180322431_AddAnotoationToCourseTable.cs
+++ CodeFirstFromExistingDatabase/CodeFirstFromExistingDatabase/Migrations/202009180322431_AddAnotoationToCourseTable.cs
@@ -7,14 +7,10 @@ namespace CodeFirstFromExistingDatabase.Migrations
     {
         public override void Up()
         {
-            AlterColumn("dbo.Courses", "Name", c => c.String(nullable: false, maxLength: 255));
-            AlterColumn("dbo.Courses", "Description", c => c.String(nullable: false, maxLength: 255));
         }
         
         public override void Down()
         {
-            AlterColumn("dbo.Courses", "Description", c => c.String(nullable: false));
-            AlterColumn("dbo.Courses", "Name", c => c.String());
         }
     }
 }
