71301930fbbc9daeff8c91d5f464c3c0eb8555cc
kainthsourav <souravkainth@outlook.com>
2020-09-18 10:12:47 +0530

    LINQ with Syntax and Extension method

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,32 @@
-﻿
+﻿using System.Linq;
 namespace Queries
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            //Linq syntax
+            var query = from c in context.Courses
+                        where c.Name.Contains("C#")
+                        orderby c.Name
+                        select c;
+            //foreach (var item in query)
+            //{
+            //    System.Console.WriteLine(item.Name);
+            //}
+
+            //Extention menthod
+            var courses = context.Courses.Where(c => c.Name.Contains("C#")).OrderBy(c => c.Name);
+            foreach (var item in courses)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+            {
+
+            }
+
         }
     }
 }
