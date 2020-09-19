e6d9485e070882921f2d407d726901b34607e9b2
kainthsourav <souravkainth@outlook.com>
2020-09-19 15:14:37 +0530

    Lazy,Eager,Explicit Loading

---
+++ Vidzy_2/Vidzy/Vidzy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Data.Entity;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Lazy loading
+            var context = new VidzyContext();
+            var videos = context.Videos.ToList();
+
+            foreach (var item in videos)
+            {
+                System.Console.WriteLine("{0}{1}", item.Genre, item.Name);
+              
+            }
+
+            //Eager Loading
+            var vid = context.Videos.Include(v => v.Genre).ToList();
+            foreach (var item in vid)
+            {
+                System.Console.WriteLine("{0}{1}", item.Name, item.Genre.Name);
+            }
+
+            //Explicit
+            var vidd = context.Videos.ToList();
+            context.Genres.Load();
+            foreach (var item in vidd)
+            {
+                System.Console.WriteLine("{0}{1}", item.Name, item.Genre.Name);
+            }
+        }
+    }
+}
